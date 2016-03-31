06f6e838c2a29103f805a0ec7f5ef90c57f2313f
Cloud <huating558@163.com>
2016-03-31 18:01:36 +0800

    添加sliding menu

--- app/src/main/java/cloud/cn/applicationtest/activity/MainActivity.java
+++ app/src/main/java/cloud/cn/applicationtest/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package cloud.cn.applicationtest.activity;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 
 import cloud.cn.androidlib.activity.BaseActivity;
 import cloud.cn.applicationtest.R;
@@ -12,6 +16,9 @@ import cloud.cn.applicationtest.R;
  */
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity{
+    @ViewInject(R.id.vp_main)
+    private ViewPager vp_main;
+
     @Override
     protected void initVariables() {
 
@@ -19,7 +26,16 @@ public class MainActivity extends BaseActivity{
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
+        initSlidingMenu();
+    }
 
+    private void initSlidingMenu() {
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.menu_sliding_layout);
     }
 
     @Override
